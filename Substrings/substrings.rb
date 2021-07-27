e9eaff6affae6f3a229f86f3064b4f1984a2c8ab
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-07-26 19:11:49 -0600

    Finish substrings project
    - Use reduce on the dictionary array to get the final hash
    - Add a docstring with an example

--- Substrings/substrings.rb
+++ Substrings/substrings.rb
@@ -1,3 +1,21 @@
-def substrings(substr, dictionary)
-    substr
+# Create a hash of substrings mapping to their fequencies in the
+# given string.
+#
+#   substrings("Well of course I know him; he's me! That's a meme.", ["our", "well", "me"]) 
+#      #=> {"our" => 1, "well" => 1, "me" => 3}
+#
+def substrings(str, dictionary)
+    # Make case insensitive for the input string.
+    downcase_str = str.downcase
+    dictionary.reduce(Hash.new(0)) do |accumulator, word|
+        # Make case insensitive for the dictionary
+        downcase_word = word.downcase
+        word_frequency = downcase_str.gsub(downcase_word).to_a.length
+        
+        if word_frequency > 0
+            accumulator[downcase_word] = word_frequency
+        end
+
+        accumulator
+    end
 end
