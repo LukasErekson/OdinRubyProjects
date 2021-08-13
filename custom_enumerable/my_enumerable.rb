a49141855e291436cf11fedc61ec929c0554c68c
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-08-12 21:10:10 -0600

    Begin TDD of Custom Enumerable Methods
    - Create the RSpec tests for several methods
    - Implement the methods and ensure they pass
      the tests

--- custom_enumerable/my_enumerable.rb
+++ custom_enumerable/my_enumerable.rb
@@ -1,18 +1,69 @@
 # frozen_string_literal: true
 
+# Extension of the Enumerable module that re-implements a lot of Enumerable's
+# key methods.
 module Enumerable
+  ##
+  # Replicates the behavior of Enumerable.each
   def my_each
     return self unless block_given?
 
-    if self.is_a? Array
-      self.length.times { |idx| yield(self[idx]) }
-    elsif self.is_a? Hash
+    if is_a? Array
+      length.times { |idx| yield(self[idx]) }
+    elsif is_a? Hash
       keys = self.keys
-      vals = self.values
-      self.length.times { |idx| yield(keys[idx], vals[idx]) }
+      vals = values
+      length.times { |idx| yield(keys[idx], vals[idx]) }
     else
       warn 'Incompatible type!'
     end
     self
   end
+
+  ##
+  # Replicates the behavior of Enumerable.each_with_index
+  def my_each_with_index
+    return self unless block_given?
+
+    if is_a? Array
+      length.times { |idx| yield(self[idx], idx) }
+    elsif is_a? Hash
+      keys = self.keys
+      vals = values
+      length.times { |idx| yield([keys[idx], vals[idx]], idx) }
+    else
+      warn 'Incompatible type!'
+    end
+    self
+  end
+
+  ##
+  # Replicates the behavior of Enumerable.select
+  def my_select
+    return self unless block_given?
+
+    result = self.class.new
+
+    if is_a? Array
+      my_each { |val| result.push(val) if yield(val) }
+    elsif is_a? Hash
+      my_each { |key, val| result[key] = val if yield(key, val) }
+    else
+      warn 'Incompatible type!'
+    end
+    result
+  end
+
+  ##
+  # Replicates the behavior of Enumerable.all?
+  def my_all?(&block)
+    return true unless block_given?
+
+    
+
+    return self.my_select(&block) == self
+  end
+
+  ##
+  # Replicates the behavior of Enumerable.any?
 end
--- custom_enumerable/spec/enumerable_custom.rb
+++ custom_enumerable/spec/enumerable_custom.rb
@@ -7,33 +7,218 @@ RSpec.describe 'Custom Enumerable Methods' do
   num_array = [1, 3, 4, 5]
   str_array = %w[this is my array of words]
   hash = { name: 'hash', color: 'red', easy: false }
+  nested_array = [[1, 2], [3, 4, 5]]
+  nested_hash = { first: { name: 'flash', power: 'speed' },\
+                  second: { name: 'grog', power: 'himbo' } }
 
-  describe 'each method' do
-    it 'prints number arrays' do
-      expect($stdout).to receive(:puts).with(1)
-      expect($stdout).to receive(:puts).with(3)
-      expect($stdout).to receive(:puts).with(4)
-      expect($stdout).to receive(:puts).with(5)
-      num_array.my_each { |val| puts val }
-    end
+  describe '1.#my_each method' do
+    context 'with example structures:' do
+      it 'works with integer arrays' do
+        num_array.each do |num|
+          expect($stdout).to receive(:puts).with(num)
+        end
+        num_array.my_each { |val| puts val }
+      end
 
-    it 'prints string arrays' do
-      expect($stdout).to receive(:puts).with('this')
-      expect($stdout).to receive(:puts).with('is')
-      expect($stdout).to receive(:puts).with('my')
-      expect($stdout).to receive(:puts).with('array')
-      expect($stdout).to receive(:puts).with('of')
-      expect($stdout).to receive(:puts).with('words')
-      str_array.my_each { |word| puts word }
-    end
+      it 'works with string arrays' do
+        str_array.each do |str|
+          expect($stdout).to receive(:puts).with(str)
+        end
+        str_array.my_each { |word| puts word }
+      end
 
-    it 'prints hash values and keys' do
-      expect($stdout).to receive(:puts).with('name : hash')
-      expect($stdout).to receive(:puts).with('color : red')
-      expect($stdout).to receive(:puts).with('easy : false')
-      hash.my_each do |key, value|
-        puts "#{key} : #{value}"
+      it 'works with hash values and keys' do
+        hash.each do |key, value|
+          expect($stdout).to receive(:puts).with("#{key} : #{value}")
+        end
+        hash.my_each do |key, value|
+          puts "#{key} : #{value}"
+        end
       end
     end
+
+    context 'with edge cases:' do
+      it 'works with empty arrays' do
+        [].each do |n|
+          expect($stdout).to receive(:puts).with(n)
+        end
+        expect([].my_each { |n| puts n }).to eq([])
+      end
+
+      it 'works with empty hashes' do
+        {}.each do |_k, _v|
+          expect($stdout).to receive(:puts).with(o)
+        end
+        expect({}.my_each { |k, _v| puts k }).to eq({})
+      end
+    end
+
+    context 'with nested structures:' do
+      it 'works with nested arrays' do
+        nested_array.each { |n| expect($stdout).to receive(:puts).with(n) }
+        expect(nested_array.my_each { |val| puts val }).to eq(nested_array)
+      end
+
+      it 'works with nested hashes' do
+        nested_hash.each { |k, v| expect($stdout).to receive(:puts).with("#{k}:#{v}") }
+        expect(nested_hash.my_each { |k, v| puts "#{k}:#{v}" }).to eq(nested_hash)
+      end
+    end
+  end
+
+  describe '2.#my_each_with_index method' do
+    context 'with example structures:' do
+      it 'prints integer arrays with indices' do
+        num_array.each_with_index { |val, idx| expect($stdout).to receive(:puts).with("#{idx}:#{val}") }
+        num_array.my_each_with_index { |val, idx| puts "#{idx}:#{val}" }
+      end
+
+      it 'prints string arrays with indices' do
+        str_array.each_with_index { |val, idx| expect($stdout).to receive(:puts).with("#{idx}:#{val}") }
+        str_array.my_each_with_index { |val, idx| puts "#{idx}:#{val}" }
+      end
+
+      it 'prints hash pairs and indices' do
+        hash.each_with_index do |pair, idx|
+          expect($stdout).to receive(:puts).with("#{pair} : #{idx}")
+        end
+        hash.my_each_with_index do |pair, idx|
+          puts "#{pair} : #{idx}"
+        end
+      end
+    end
+
+    context 'with edge cases:' do
+      it 'works with empty arrays' do
+        [].each_with_index do |val, idx|
+          expect($stdout).to receive(:puts).with("#{idx}:#{val}")
+        end
+        [].my_each_with_index { |val, idx| "#{idx}:#{val}" }
+      end
+
+      it 'works with empty hashes' do
+        {}.each_with_index do |val, idx|
+          expect($stdout).to receive(:puts).with("#{idx}:#{val}")
+        end
+        {}.my_each_with_index { |val, idx| "#{idx}:#{val}" }
+      end
+    end
+
+    context 'with nested structures:' do
+      it 'works with nested arrays' do
+        nested_array.each_with_index { |val, idx| expect($stdout).to receive(:puts).with("#{idx}:#{val}") }
+        nested_array.my_each_with_index { |val, idx| puts "#{idx}:#{val}" }
+      end
+
+      it 'works with nested hashes' do
+        nested_hash.each_with_index { |val, idx| expect($stdout).to receive(:puts).with("#{idx}:#{val}") }
+        nested_hash.my_each_with_index { |val, idx| puts "#{idx}:#{val}" }
+      end
+    end
+  end
+
+  describe '3.#my_select method' do
+    context 'with example structures:' do
+      it 'selects from integer arrays' do
+        select_criterion = ->(number) { number.between?(2, 5) }
+        expected_output = num_array.select { |val| select_criterion.call(val) }
+        expect(num_array.my_select { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'selects from string arrays' do
+        select_criterion = ->(str) { str.include?('y') }
+        expected_output = str_array.select { |val| select_criterion.call(val) }
+        expect(str_array.my_select { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'selects from hashes' do
+        select_criterion = ->(_key, val) { val.is_a? String }
+        expected_output = hash.select { |key, val| select_criterion.call(key, val) }
+        expect(hash.my_select { |key, val| select_criterion.call(key, val) }).to eq(expected_output)
+      end
+    end
+
+    context 'with edge cases:' do
+      it 'works with empty arrays' do
+        select_criterion = ->(val) { val.nil? }
+        expected_output = [].select { |val| select_criterion.call(val) }
+        expect([].my_select { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'works with empty hashes' do
+        select_criterion = ->(_key, val) { val.nil? }
+        expected_output = {}.select { |key, val| select_criterion.call(key, val) }
+        expect({}.my_select { |key, val| select_criterion.call(key, val) }).to eq(expected_output)
+      end
+    end
+
+    context 'with nested structures:' do
+      it 'works with nested arrays' do
+        select_criterion = ->(val) { val.length > 2 }
+        expected_output = nested_array.select { |val| select_criterion.call(val) }
+        expect(nested_array.my_select { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'works with nested hashes' do
+        select_criterion = ->(_key, val) { val.length > 2 }
+        expected_output = nested_hash.select { |key, val| select_criterion.call(key, val) }
+        expect(nested_hash.my_select { |key, val| select_criterion.call(key, val) }).to eq(expected_output)
+      end
+    end
+  end
+
+  describe '4.#my_all method' do
+
+    context 'with example structures' do
+      it 'works with integer array' do
+        criterion = -> (num) { num.is_a? Integer }
+        shared_block = Proc.new { |num| criterion.call(num) }
+        expected_output = num_array.all?(&shared_block)
+        expect(num_array.my_all?(&shared_block)).to eq(expected_output)
+      end
+
+      it 'works with string array' do
+        criterion = -> (str) { str.length > 2 }
+        shared_block = Proc.new { |str| criterion.call(str) }
+        expected_output = str_array.all?(&shared_block)
+        expect(str_array.my_all?(&shared_block)).to eq(expected_output)
+      end
+
+      it 'works with hash' do
+        criterion = -> (key, _val) { key.is_a? Symbol }
+        shared_block = Proc.new { |key, _val| criterion.call(key, _val) }
+        expected_output = hash.all?(&shared_block)
+        expect(hash.my_all?(&shared_block)).to eq(expected_output)
+      end
+    end
+
+    context 'with edge cases:' do
+      it 'works with empty arrays' do
+        select_criterion = ->(val) { val.nil? }
+        expected_output = [].all? { |val| select_criterion.call(val) }
+        expect([].my_all? { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'works with empty hashes' do
+        select_criterion = ->(_key, val) { val.nil? }
+        expected_output = {}.all? { |key, val| select_criterion.call(key, val) }
+        expect({}.my_all? { |key, val| select_criterion.call(key, val) }).to eq(expected_output)
+      end
+    end
+
+    context 'with nested structures:' do
+      it 'works with nested arrays' do
+        select_criterion = ->(val) { val.length > 2 }
+        expected_output = nested_array.all? { |val| select_criterion.call(val) }
+        expect(nested_array.my_all? { |val| select_criterion.call(val) }).to eq(expected_output)
+      end
+
+      it 'works with nested hashes' do
+        select_criterion = ->(_key, val) { val.length > 2 }
+        expected_output = nested_hash.all? { |key, val| select_criterion.call(key, val) }
+        expect(nested_hash.my_all? { |key, val| select_criterion.call(key, val) }).to eq(expected_output)
+      end
+    end
+
   end
 end
