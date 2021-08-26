22b8191295f80352f22a45cc71d75f051afaaac9
Lukas Erekson <50251026+LukasErekson@users.noreply.github.com>
2021-08-26 14:41:12 -0600

    Complete caesar_cipher_spec.rb
    Write several tests for Caesar Cipher project

--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -4,12 +4,42 @@ require_relative 'spec_helper'
 require_relative '../caesar_cipher'
 
 RSpec.describe '#caesar_cipher' do
-  let(:plain_text) { 'I have a bad feeling about this.' }
+  let(:plaintext) { 'I have a bad feeling about this.' }
+  # Catch $stderr to ensure warnings work
+  $stderr = StringIO.new
+
   context 'when given multiples of 26' do
-    it 'returns original message' do
-      4.times do |i|
-        expect(caesar_cipher(plain_text, 26*(i-2))).to eq(plain_text)
+    3.times do |i|
+      shift = 26 * (i - 2)
+
+      it "returns original message for shift = #{shift}" do
+        expect(caesar_cipher(plaintext, shift)).to eq(plaintext)
       end
+
+      it "warns about the ciphertext equaling the plaintext for shift = #{shift}" do
+        $stderr.rewind
+        expect($stderr.readlines[i].include?("The ciphertext is identical to the message. (#{shift} % 26 == 0)")).to be true
+      end
+    end
+  end
+
+  context 'when given positve shift values' do
+    it 'returns shifted ciphertext' do
+      ciphertext = 'Z yrmv r sru wvvczex rsflk kyzj.'
+      expect(caesar_cipher(plaintext, 17)).to eq ciphertext
+    end
+  end
+
+  context 'when given negative shift values' do
+    it 'returns shifted ciphertext' do
+      ciphertext = 'Z yrmv r sru wvvczex rsflk kyzj.'
+      expect(caesar_cipher(plaintext, -9)).to eq ciphertext
+    end
+  end
+
+  context 'when given punctation characters' do
+    it 'returns unlatered string' do
+      expect(caesar_cipher('.,!></?&%@#*', 5)).to eql '.,!></?&%@#*'
     end
   end
 end
